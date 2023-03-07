5f4b134a299f8b52ec8b85d6e52ffe559c7dbecf
Ghazi-Alloh <ghaziyalloh@gmail.com>
2023-03-07 04:38:24 +0000

    Initial Commit

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('',views.home),
+    path('product/',views.product),
+    path('customer/',views.customer),
+    
+    
+]
